ded654471ec30814739464176f4482fadc91bc3f
owent <owt5008137@live.com>
2015-07-10 22:15:05 +0800

    预留接口

--- src/main.js
+++ src/main.js
@@ -31,124 +31,186 @@ function alert_error(content, title) {
             run_seq: 0,
             gui: {
                 set_name: null
-            }
+            },
+            tree: [],
+            category: {},
+            file_map: {}
         };
     }
 
-    function build_conv_tree(context) {
+    function build_conv_tree(context, current_path, callback) {
         // $("#conv_list").empty();
-        reset_conv_data();
 
         // 初始化
         var jdom = $(context);
 
-        // 加载全局配置
-        $.each(jdom.children("global").children(), function(k, dom){
-            var tn = dom.tagName.toLowerCase();
-            var val = $(dom).html().trim();
+        var include_list = [];
+        // 暂时不知道怎么获取文件路径
+        //var prefix_dir = current_path.replace(/[^\\\/]*$/, "");
+        //// 加载include项目
+        //$.each(jdom.children("global").children(), function(k, dom){
+        //    var file_path = $(dom).html();
+        //    if (file_path) {
+        //        if (!file_path.match(/^(\w:|\/)/i)) {
+        //            file_path = prefix_dir + file_path;
+        //        }
+        //        include_list.push(file_path);
+        //    }
+        //});
 
-            if ("work_dir" == tn) {
-                $("#conv_list_work_dir").val(val);
-            } else if ("xresloader_path" == tn) {
-                $("#conv_list_xresloader").val(val);
-            } else if ("proto_file" == tn) {
-                $("#conv_list_proto_file").val(val);
-            }else if ("output_dir" == tn) {
-                $("#conv_list_output_dir").val(val);
-            } else if ("data_src_dir" == tn) {
-                $("#conv_list_data_src_dir").val(val);
-            } else if ("rename" == tn) {
-                $("#conv_list_rename").val(val);
-            } else if ("proto" == tn) {
-                $("#conv_list_protocol").get(0).selectedIndex = $("#conv_list_protocol option[value=" + val + "]").get(0).index;
-            } else if ("output_type" == tn) {
-                $("#conv_list_output_type").get(0).selectedIndex = $("#conv_list_output_type option[value=" + val + "]").get(0).index;
-            } else if ("option" == tn && val) {
-                conv_data.global_options.push({
-                    name: $(dom).attr('name') || val,
-                    desc: $(dom).attr('desc') || val,
-                    value: val
-                });
-            }
-        });
+        var active_run = (function(){
+            // 加载并覆盖全局配置
+            $.each(jdom.children("global").children(), function(k, dom){
+                var tn = dom.tagName.toLowerCase();
+                var val = $(dom).html().trim();
 
-        // 加载分类信息
-        var treeData = [];
-        var cat_map = {};
-        function build_tree_fn(root, xml_dom) {
-            $.each($(xml_dom).children("tree"), function(k, xml_node) {
-                var nj_node = $(xml_node);
-                var new_option = {
-                    title: nj_node.attr("name") || nj_node.attr("id"),
-                    tooltip: nj_node.attr("name") || nj_node.attr("id"),
-                    folder: true,
-                    children: []
-                };
-
-                if (nj_node.attr('id')) {
-                    cat_map[nj_node.attr('id')] = new_option;
+                if ("work_dir" == tn) {
+                    $("#conv_list_work_dir").val(val);
+                } else if ("xresloader_path" == tn) {
+                    $("#conv_list_xresloader").val(val);
+                } else if ("proto_file" == tn) {
+                    $("#conv_list_proto_file").val(val);
+                }else if ("output_dir" == tn) {
+                    $("#conv_list_output_dir").val(val);
+                } else if ("data_src_dir" == tn) {
+                    $("#conv_list_data_src_dir").val(val);
+                } else if ("rename" == tn) {
+                    $("#conv_list_rename").val(val);
+                } else if ("proto" == tn) {
+                    $("#conv_list_protocol").get(0).selectedIndex = $("#conv_list_protocol option[value=" + val + "]").get(0).index;
+                } else if ("output_type" == tn) {
+                    $("#conv_list_output_type").get(0).selectedIndex = $("#conv_list_output_type option[value=" + val + "]").get(0).index;
+                } else if ("option" == tn && val) {
+                    conv_data.global_options.push({
+                        name: $(dom).attr('name') || val,
+                        desc: $(dom).attr('desc') || val,
+                        value: val
+                    });
                 }
-
-                build_tree_fn(new_option.children, nj_node);
-                root.push(new_option);
             });
-        };
-        build_tree_fn(treeData, jdom.children("category"));
 
-        // GUI 显示规则
-        $.each(jdom.children("gui").children("set_name"), function(k, dom){
-            conv_data.gui.set_name = eval($(dom).html());
-        });
+            // 加载分类信息
+            var treeData = conv_data.tree;
+            var cat_map = conv_data.category;
+            function build_tree_fn(root, xml_dom) {
+                $.each($(xml_dom).children("tree"), function(k, xml_node) {
+                    var nj_node = $(xml_node);
+                    var new_option = {
+                        title: nj_node.attr("name") || nj_node.attr("id"),
+                        tooltip: nj_node.attr("name") || nj_node.attr("id"),
+                        folder: true,
+                        children: []
+                    };
 
-        $.each(jdom.children("list").children("item"), function(k, item_node) {
-            var jitem = $(item_node);
-            var id = generate_id();
+                    if (nj_node.attr('id')) {
+                        cat_map[nj_node.attr('id')] = new_option;
+                    }
 
-            var item_data = {
-                id: id,
-                file: jitem.attr('file'),
-                scheme: jitem.attr('scheme'),
-                name: (jitem.attr('name').trim() || ""),
-                cat: jitem.attr('cat'),
-                options: [],
-                desc: (jitem.attr('name').trim() || jitem.attr('desc').trim() || "")  + " -- 文件名: \"" + jitem.attr("file") + "\" 描述信息: \"" + jitem.attr("scheme") + "\""
+                    build_tree_fn(new_option.children, nj_node);
+                    root.push(new_option);
+                });
             };
+            build_tree_fn(treeData, jdom.children("category"));
 
             // GUI 显示规则
-            if (conv_data.gui.set_name) {
-                try {
-                    item_data = conv_data.gui.set_name(item_data) || item_data;
-                } catch (err) {
-                    assert("ERROR: " + err.toString());
-                }
-            }
-
-            $.each(jitem.children('option'), function(k, v){
-                var nj_node = $(v);
-                item_data.options.push({
-                    name: nj_node.attr('name'),
-                    desc: nj_node.attr('desc'),
-                    value: nj_node.html()
-                });
+            $.each(jdom.children("gui").children("set_name"), function(k, dom){
+                conv_data.gui.set_name = eval($(dom).html());
             });
-            conv_data.items[item_data.id] = item_data;
 
-            var ft_node = {
-                title: item_data.name,
-                tooltip: item_data.desc,
-                key: item_data.id
-            };
-            if (item_data.cat && cat_map[item_data.cat]) {
-                cat_map[item_data.cat].children.push(ft_node);
-            } else {
-                treeData.push(ft_node);
+            $.each(jdom.children("list").children("item"), function(k, item_node) {
+                var jitem = $(item_node);
+                var id = generate_id();
+
+                var item_data = {
+                    id: id,
+                    file: jitem.attr('file'),
+                    scheme: jitem.attr('scheme'),
+                    name: (jitem.attr('name').trim() || ""),
+                    cat: jitem.attr('cat'),
+                    options: [],
+                    desc: (jitem.attr('name').trim() || jitem.attr('desc').trim() || "")  + " -- 文件名: \"" + jitem.attr("file") + "\" 描述信息: \"" + jitem.attr("scheme") + "\""
+                };
+
+                // GUI 显示规则
+                if (conv_data.gui.set_name) {
+                    try {
+                        item_data = conv_data.gui.set_name(item_data) || item_data;
+                    } catch (err) {
+                        assert("ERROR: " + err.toString());
+                    }
+                }
+
+                $.each(jitem.children('option'), function(k, v){
+                    var nj_node = $(v);
+                    item_data.options.push({
+                        name: nj_node.attr('name'),
+                        desc: nj_node.attr('desc'),
+                        value: nj_node.html()
+                    });
+                });
+                conv_data.items[item_data.id] = item_data;
+
+                var ft_node = {
+                    title: item_data.name,
+                    tooltip: item_data.desc,
+                    key: item_data.id
+                };
+                if (item_data.cat && cat_map[item_data.cat]) {
+                    cat_map[item_data.cat].children.push(ft_node);
+                } else {
+                    treeData.push(ft_node);
+                }
+            });
+
+            if (callback) {
+                callback();
             }
         });
 
+
+        var load_one_by_one = {fn: null};
+        load_one_by_one.fn = function () {
+            var file_path = null;
+            while (include_list.length > 0) {
+                file_path = include_list.shift();
+
+                if (conv_data.file_map[file_path]) {
+                    alert("文件" + file_path + " 已被加载过，不能循环include文件");
+                    file_path = null;
+                } else {
+                    break;
+                }
+            }
+
+            if (file_path) {
+                var file_loader = new FileReader();
+
+                file_loader.onload = (function(ev) {
+                    build_conv_tree(ev.target.result, file_path);
+                });
+
+                file_loader.onerror = function(ev) {
+                    alert("尝试读取文件失败:" +　file_path);
+                };
+
+                file_loader.onloadend = function(){
+                    load_one_by_one.fn();
+                };
+
+                file_loader.readAsText(file_path);
+            } else {
+                active_run();
+            }
+        };
+
+        load_one_by_one.fn();
+    }
+
+    function show_conv_tree() {
         $("#conv_list").fancytree({
             checkbox: true,
             selectMode: 3,
-            source: treeData,
+            source: conv_data.tree,
             dblclick: function(event, data) {
                 data.node.toggleSelected();
             },
@@ -254,16 +316,27 @@ function alert_error(content, title) {
 
         $("#conv_list_file").bind("change", function(){
             $("#conv_list_file_val").val($(conv_list_file).val());
-            
+
+            var sel_dom = document.getElementById("conv_list_file");
+            var file_path = sel_dom.files.length > 0? sel_dom.files[0]: null;
+
             var file_loader = new FileReader();
 
             file_loader.onload = function(ev) {
-                build_conv_tree(ev.target.result);
-            }
+                reset_conv_data();
+                build_conv_tree(ev.target.result, file_path, function(){
+                    // 显示属性树
+                    show_conv_tree();
+                });
+            };
 
-            var sel_dom = document.getElementById("conv_list_file");
-            if (sel_dom.files.length > 0) {
-                file_loader.readAsText(sel_dom.files[0]);
+            file_loader.onerror = function(ev) {
+                alert("尝试读取文件失败:" +　file_path);
+            };
+
+            if (file_path) {
+                // conv_data.file_map[file_path] = true;
+                file_loader.readAsText(file_path);
             }
         });
 
